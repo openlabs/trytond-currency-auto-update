bebf5000db84c7bae250ae893470400ef4b580f6
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-05-23 09:37:59 -0400

    Updated License and terms

--- __tryton__.py
+++ __tryton__.py
@@ -8,7 +8,7 @@
     'author': 'Openlabs Technologies & Consulting (P) LTD',
     'email':'info@openlabs.co.in',
     'website': 'http://www.openlabs.co.in/',
-    'description': '''Auto update of Currency
+    'description': '''Auto update of Currency Exchange Rate
 
     The module fetches rates from the internet and updates the Tryton
     rates for currencies enabled to use the auto update.
@@ -21,9 +21,9 @@
     providers identified by Openlabs and maybe delayed. All information 
     provided "as is" for informational purposes only, not intended for 
     trading purposes or advice. Openlabs or any of independent providers 
-    is liable for any informational errors, incompleteness, or delays, or 
-    for any actions taken in reliance on information contained herein.
-    
+    is not liable for any informational errors, incompleteness, or delays, 
+    or for any actions taken in reliance on information contained herein.
+
     By installing the module , you agree to the above terms and conditions.
     ''',
     'depends' : [
